93df0eed5319ca3924c45ee6c61295b172a09f85
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2022-11-18 09:35:44 +0530

    Update Exp_5.sql

--- Exp_5.sql
+++ Exp_5.sql
@@ -8,3 +8,16 @@ SELECT COUNT(*) FROM Book_Author WHERE Author_Id=1;
 +----------+
 */
 --b
+SELECT Publisher.Name,COUNT(*)  FROM Publisher,BOOK WHERE Publisher.Publisher_id=BOOK.Publisher_id GROUP BY(Publisher.Publisher_id);
+/*
++---------+----------+
+| Name    | COUNT(*) |
++---------+----------+
+| Amal    |        1 |
+| Sathyan |        1 |
+| Vinny   |        1 |
+| Manu    |        2 |
+| Ram     |        1 |
++---------+----------+
+*/
+--C
