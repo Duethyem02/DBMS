4f8c8143fb8bed05890f9df372db3b287e9a9b1f
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2022-12-02 10:37:23 +0530

    Update EXP_8.sql

--- EXP_8.sql
+++ EXP_8.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE TRANSACTION;
 USE TRANSACTION;
-CREATE TABLE Book_Issue(Issue_id int,Date_of_issue date,Book_id int,Member_id int,Expected_date_of_return date,PRIMARY KEY(Issue_id));
+CREATE TABLE Book_Issue(Issue_id int,Date_of_issue date,Book_id int,Status varchar(20),Expected_date_of_return date,PRIMARY KEY(Issue_id));
 CREATE TABLE Book_Return(Issue_id int,Actual_date_of_return date,late_days int,late_fee int,FOREIGN KEY(Issue_id) REFERENCES Book_Issue(Issue_id));
