75443dc035d08354a9667f0870d8d3c3a542670f
Duethy Elsa Mathews <77284995+Duethyem02@users.noreply.github.com>
2022-12-02 12:31:12 +0530

    Update EXP_10.sql

--- EXP_10.sql
+++ EXP_10.sql
@@ -1,1 +1,4 @@
-
+CREATE PROCEDURE Operation(IN Book_id INT,IN Title VARCHAR(20),IN Language_id INT,IN MRP INT,IN Publisher_id INT,IN Published_date DATE,IN Volume INT,IN
+Status VARCHAR(20),IN op INT)
+    -> BEGIN
+    -> IF 
